191c438fe40041fea7b26f0edeffc6b7b4b68d2a
Dmitry Uzunov <duyzillio@gmail.com>
2024-03-03 20:56:02 +0300

    level 3

--- level_3/2.py
+++ level_3/2.py
@@ -1,16 +1,24 @@
-from constants import ___
+from typing import TypedDict
 
 
-def calculate_total_spent_for_user(user: ___) -> ___:
-    # попробуй тут воспользовать typing.TypedDict
-    pass
+class User(TypedDict):
+    name: str
+    age: int
+    transactions_sums: list[int]
 
 
-if __name__ == "__main__":
-    assert calculate_total_spent_for_user(
-        user={
-            "name": "Ilya",
-            "age": 32,
-            "transactions_sums": [102, 15, 63, 12],
-        },
-    ) == 192
+def calculate_total_spent_for_user(user: User) -> int:
+    return sum(user.get('transactions_sums', []))
+
+
+if __name__ == '__main__':
+    assert (  # noqa: S101
+        calculate_total_spent_for_user(
+            user={
+                'name': 'Ilya',
+                'age': 32,
+                'transactions_sums': [102, 15, 63, 12],
+            },
+        )
+        == 192  # noqa: PLR2004
+    )
